aca0c571f9faa90c81ff59b3203ea6ddbf356238
Patricio Perez Herrero <patriciopherrero@gmail.com>
2022-05-18 15:11:35 +0200

    Fished and styled

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Movies::HitchhikersGuideToTheGalaxy.character,
+    address: Faker::Movies::HitchhikersGuideToTheGalaxy.location,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save!
+
+  review = Review.new(
+    rating: Faker::Number.between(from: 1, to: 5),
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.specie
+  )
+  review.restaurant = restaurant
+  review.save!
+end
